3b9141cd25f757c387872bf41f5ad96cb57e84ac
bokusik <govardrork1@gmail.com>
2023-02-07 15:47:51 +0300

    https://www.codewars.com/kata/54e6533c92449cc251001667

--- UniqueInOrder.py
+++ UniqueInOrder.py
@@ -1,9 +1,2 @@
 def unique_in_order(sequence):
-    result = []
-    for i in range(len(sequence)):
-        try:
-            if sequence[i] != sequence[i+1]:
-                result.append(sequence[i])
-        except IndexError:
-            result.append(sequence[i])
-    return result
+    return [char for i, char in enumerate(sequence) if i == 0 or char != sequence[i - 1]]
